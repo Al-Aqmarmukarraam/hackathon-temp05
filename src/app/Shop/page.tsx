6051dffea118c645eb27ce28720dd8a38bb9fb30
Al-Aqmarmukarraam <alaqmarmukarram786@gmail.com>
2024-12-17 02:03:28 +0500

    third commit

--- src/app/Shop/page.tsx
+++ src/app/Shop/page.tsx
@@ -1,14 +1,5 @@
 "use client"
 import React from "react";
-import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-  } from "@/components/ui/pagination"
 import Navbar1 from "../Components/Navbar1";
 import Footer from "../Components/Footer";
 
@@ -208,31 +199,6 @@ const Shop = () => {
         ))}
       </div>
     </div>
-
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
     </>
   );
 };
